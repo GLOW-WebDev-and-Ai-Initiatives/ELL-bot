85d701070ba159c2f69f1cdd2f2ff1b1133cd162
NeilB18 <82923875+NeilB18@users.noreply.github.com>
2024-07-24 15:38:59 -0700

    added navbar component

--- ChatBot_prototype/src/App.tsx
+++ ChatBot_prototype/src/App.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import Chatbot from "./components/Chatbot/chatbot";
+import Navbar from "./components/navbar/navbar";
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      
+      <Navbar></Navbar>
       <Chatbot />
     </div>
   );
